969281cb21aedc896bbf985c0e02ae77d8f334fc
Xiang Chen <xiang.chen@jhu.edu>
2025-04-25 15:49:34 -0400

    Setup repo and finalize project metadata

--- fim/__init__.py
+++ fim/__init__.py
@@ -1,4 +1,4 @@
-__project__ = "package_name"  # Replace package_name with actual name (it's just a template).
+__project__ = "fim"  # Replace fim with actual name (it's just a template).
 
 try:
     from ._version import __version__
