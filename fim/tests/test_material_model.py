f07429554248632ffd2ff5f12d9c82d062cd8d69
Xiang Chen <xiang.chen@jhu.edu>
2025-08-29 12:35:16 -0400

    add legacy NH model

--- fim/tests/test_material_model.py
+++ fim/tests/test_material_model.py
@@ -1,6 +1,5 @@
 import numpy as np
 from refactor.material_model import MaterialModel
-# from refactor.vws_models import calculate_VWS_hgo, calculate_VWS_linear, sensitivity_full
 
 
 def test_linear_model_parameters():
